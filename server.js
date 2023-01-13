b7b6d0d1adebc79037a1968fad9df9233a437f60
thnbi <renato_freitas13@icloud.com>
2023-01-13 14:50:58 -0300

    utilização do express para os metodos da API

--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-import http from 'http';
+import app from "./src/app.js";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const rotas = {
-   '/': 'hello world',
-   '/skins': 'skins de valorant',
-   '/champions': 'jett>>>',
-   '/maps': 'mapas de valorant'
-}
-
-const server = http.createServer((req, res) => {
-   res.writeHead(200, { 'Content-Type': 'text/plain' });
-   res.end(rotas[req.url] || 'Page not found!');
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
    console.log(`Server running at http://localhost:${port}`);
 });
