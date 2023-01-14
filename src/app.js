c79f3084d53dd23871cc1a366108a9c07dc26e65
thnbi <renato_freitas13@icloud.com>
2023-01-14 10:38:14 -0300

    melhorando estilo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js"
-import agents from "./models/Agent.js"
 import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, "connection error"))
@@ -10,9 +9,7 @@ db.once("open", () => {
 })
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
 
 export default app
